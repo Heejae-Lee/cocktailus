dd853713aaaed2b031f5b124ea1713ada58f385c
Heejae-Lee <gmlwo1340@naver.com>
2021-07-21 11:30:10 +0900

    Feat: '리액트 초기 세팅'

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <img className="logo" src={logo} alt="logo"/>
+      <br/>
+      Do you wanna cocktail with us?
+    </div>
+  );
+}
+
+export default App;
