b12725013bb3ccea71bd5460b7caa9998d971bcb
lktgt15 <lktgt15@gmail.com>
2021-07-21 11:03:44 +0900

    Feat: 스프링 초기 세팅

---
+++ backend/cocktailer/src/main/java/com/iot/cocktailer/CocktailerApplication.java
@@ -0,0 +1,13 @@
+package com.iot.cocktailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailerApplication.class, args);
+	}
+
+}
---
+++ backend/cocktailer/src/test/java/com/iot/cocktailer/CocktailerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot.cocktailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
