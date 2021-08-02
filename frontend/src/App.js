386c0abcb155637c1398e55f6e2dfdc1cb4a0c43
이희재 <gmlwo1340@naver.com>
2021-08-02 21:00:14 +0900

    Feature: '공지사항 작성 페이지 구현'
    공지사항 작성을 위한 페이지 골격 구현 및 404 오류시 출력할 페이지 다소 수정(경고 제거)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,9 +9,10 @@ import SignIn from './pages/SignIn/'
 import SignUp from './pages/SignUp/'
 import Terms from './pages/Terms/'
 import Recipe from './pages/Recipe/'
-import ErrorNotFound from './pages/ErrorNotFound/'
+import RecipeDetail from './pages/RecipeDetail';
 import RecipeAddForm from './pages/RecipeAddForm';
-
+import PageNotFound from './pages/PageNotFound'
+import WriteNotice from './pages/WriteNotice'
 
 function App() {
   return (
@@ -19,13 +20,15 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/recipe" component={Recipe} />
+        <Route exact path="/recipe/detail" component={RecipeDetail} />
+        <Route exact path="/recipe/write" component={RecipeAddForm} />
         <Route exact path="/ForgotPassword" component={ForgotPassword} />
         <Route exact path="/SignUp" component={SignUp} />
         <Route exact path="/SignIn" component={SignIn} />
         <Route exact path="/Privacy" component={Privacy} />
         <Route exact path="/Terms" component={Terms} />
-        <Route exact path="/error" component={ErrorNotFound} />
-        <Route exact path="/recipe/write" component={RecipeAddForm} />
+        <Route exact path="/error" component={PageNotFound} />
+        <Route exact path="/notice/write" component={WriteNotice} />
         <Redirect to="/error"/>
       </Switch>
     </BrowserRouter>
