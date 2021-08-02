01786978e74d8b93e5c0b94089e7dd642c8d2cc0
lktgt15 <lktgt15@gmail.com>
2021-08-02 19:07:00 +0900

    Chore: 'RecipeArticleController 변경'

--- backend/cocktailer/src/main/java/com/iot/cocktailer/controller/RecipeArticleController.java
+++ backend/cocktailer/src/main/java/com/iot/cocktailer/controller/RecipeArticleController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,7 @@ public class RecipeArticleController {
         this.jpaRecipeArticleRepository = jpaRecipeArticleRepository;
     }
 
+    @GetMapping
     public ResponseEntity getRecipes(){
         List<RecipeArticle> recipeArticles = jpaRecipeArticleRepository.findAllRecipeArticles();
         return new ResponseEntity<>(jpaRecipeArticleRepository.findAllRecipeArticles(), HttpStatus.OK);
