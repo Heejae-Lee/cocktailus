7530cd7c40d3966cbdb35771189e919d5cd61a06
lktgt15 <lktgt15@gmail.com>
2021-08-05 13:59:34 +0900

    Chore: '안쓰는 변수들 정리'

--- backend/cocktailer/src/main/java/com/iot/cocktailer/config/SecurityConfig.java
+++ backend/cocktailer/src/main/java/com/iot/cocktailer/config/SecurityConfig.java
@@ -4,10 +4,14 @@ import com.iot.cocktailer.filter.JwtAuthenticationFilter;
 import com.iot.cocktailer.service.JwtTokenService;
 import com.iot.cocktailer.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,6 +21,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
--- backend/cocktailer/src/main/java/com/iot/cocktailer/controller/NoticeController.java
+++ backend/cocktailer/src/main/java/com/iot/cocktailer/controller/NoticeController.java
@@ -8,11 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/notices")
+@CrossOrigin(origins = "*")
 public class NoticeController {
     private final JpaNoticeRepository jpaNoticeRepository;
     private final NoticeService noticeService;
