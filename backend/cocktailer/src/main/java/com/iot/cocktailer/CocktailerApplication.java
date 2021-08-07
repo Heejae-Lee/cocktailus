12202cf86908da5b834960fb8d42aab75e0ab52f
lktgt15 <lktgt15@gmail.com>
2021-08-07 22:32:32 +0900

    Feat: 'AWS S3 이미지 업로드'

--- backend/cocktailer/src/main/java/com/iot/cocktailer/CocktailerApplication.java
+++ backend/cocktailer/src/main/java/com/iot/cocktailer/CocktailerApplication.java
@@ -2,12 +2,19 @@ package com.iot.cocktailer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CocktailerApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location=" +
+			"classpath:application.properties," +
+			"classpath:aws.properties";
+
 	public static void main(String[] args) {
-		SpringApplication.run(CocktailerApplication.class, args);
+		new SpringApplicationBuilder(CocktailerApplication.class)
+							.properties(APPLICATION_LOCATIONS)
+							.run(args);
 	}
 
 }
