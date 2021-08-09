44ebd2a29e7eecfdf1f9c31436665e7f24ed1327
lktgt15 <lktgt15@gmail.com>
2021-08-09 15:19:14 +0900

    Refactor: 'Controller Service 일관적으로 분리'

--- backend/cocktailer/src/main/java/com/iot/cocktailer/controller/CommentController.java
+++ backend/cocktailer/src/main/java/com/iot/cocktailer/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.iot.cocktailer.controller;
 
 import com.iot.cocktailer.domain.Comment;
 import com.iot.cocktailer.repository.JpaCommentRepository;
+import com.iot.cocktailer.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,17 +12,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/comments")
 @CrossOrigin(origins = "*")
 public class CommentController {
-    private final JpaCommentRepository jpaCommentRepository;
+    private final CommentService commentService;
 
     @Autowired
-    public CommentController(JpaCommentRepository jpaCommentRepository){
-        this.jpaCommentRepository = jpaCommentRepository;
+    public CommentController(CommentService commentService){
+        this.commentService = commentService;
     }
 
     @PostMapping
     public ResponseEntity postComment(@RequestBody Comment comment){
-        jpaCommentRepository.save(comment);
-        return new ResponseEntity<>(comment, HttpStatus.CREATED);
+        return new ResponseEntity<>(commentService.postComment(comment), HttpStatus.CREATED);
     }
 
 }
--- backend/cocktailer/src/main/java/com/iot/cocktailer/service/CommentService.java
+++ backend/cocktailer/src/main/java/com/iot/cocktailer/service/CommentService.java
@@ -1,7 +1,22 @@
 package com.iot.cocktailer.service;
 
+import com.iot.cocktailer.domain.Comment;
+import com.iot.cocktailer.repository.JpaCommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
+    private final JpaCommentRepository jpaCommentRepository;
+
+    @Autowired
+    public CommentService(JpaCommentRepository jpaCommentRepository){
+        this.jpaCommentRepository = jpaCommentRepository;
+    }
+
+
+    public Comment postComment(Comment comment){
+        jpaCommentRepository.save(comment);
+        return comment;
+    }
 }
