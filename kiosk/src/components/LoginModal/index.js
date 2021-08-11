2c8668ab1ea0e59b715de954bbbb737610890c8a
이희재 <gmlwo1340@naver.com>
2021-08-11 17:29:48 +0900

    Feat: 'Login 시 키보드 컴포넌트 모달 창으로 나타나게 수정'

--- kiosk/src/components/LoginModal/index.js
+++ kiosk/src/components/LoginModal/index.js
@@ -2,46 +2,68 @@
 import useStyles from "./styles";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
+import "react-simple-keyboard/build/css/index.css";
 // 컴포넌트 관련
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Modal from "@material-ui/core/Modal";
+import Keyboard from "react-simple-keyboard";
 // 기능 관련
 import { userAPI } from "../../utils/axios";
 
 export default function LoginModal(props) {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
+  const [keyboardOpen, setKeyboardOpen] = React.useState({
+    target: null,
+    open: false,
+  });
   const [sent, setSent] = React.useState(false);
   const [member, setMember] = React.useState({
-      email: "",
-      password: "",
-  })
+    email: "",
+    password: "",
+  });
 
-  const handleOpen = () => {
+  // 로그인 버튼을 누르면 모달창이 활성화 됨
+  const handleOpen = (e) => {
     setOpen(true);
   };
 
+  // 모달의 바깥쪽을 클릭하면 모달창이 비활성화 됨
   const handleClose = () => {
     setOpen(false);
   };
 
-  const changeEmail = (e) => {
-    setMember({
-        ...member,
-        email: e.target.value,
-    })
-  }
+  // 입력창을 클릭하면 키보드 모달창이 활성화 됨
+  const handleKeyboardOpen = (e) => {
+    let newTarget = Object.assign({}, keyboardOpen);
+    newTarget = {
+      target: e.target.id,
+      open: true,
+    };
+    setKeyboardOpen(newTarget);
+  };
 
-  const changePassword = (e) => {
-    setMember({
-        ...member,
-        password: e.target.value
-    })
-  }
+  // 키보드 모달의 바깥쪽을 클릭하면 모달창이 비활성화 됨
+  const handleKeyboardClose = () => {
+    setKeyboardOpen({
+      target: null,
+      open: false,
+    });
+  };
 
-  // onSubmit : Form 태그가 제출될 때 실행되는 함수
+  // 키보드 입력을 위한 함수
+  const onChangeKeyboard = (input, e) => {
+    let newMember = Object.assign({}, member);
+    newMember = {
+      ...member,
+      [keyboardOpen.target]: input,
+    };
+    setMember(newMember);
+  };
+
+  // onSubmit : 버튼을 누르면 입력된 아이디와 비밀번호를 토대로 로그인
   const onSubmit = async () => {
     // form이 제출되면 로그인을 더 이상 수정할 수 없도록 잠금
     setSent(true);
@@ -49,11 +71,11 @@ export default function LoginModal(props) {
       email: member.email,
       password: member.password,
     };
-    
+
     // 로그인 요청
     const res = await userAPI.login(formData);
     //console.log(res);
-    
+
     if (res.status === 200) {
       const payload = {
         token: res.data["access-token"],
@@ -69,19 +91,15 @@ export default function LoginModal(props) {
       // 스낵바 띄우기
       // alert는 임시
       alert("로그인 성공!");
-      
+
       // 모달창 제거
       setOpen(false);
-
     } else {
       // 스낵바 띄우기
       // alert는 임시
       alert("로그인에 실패하였습니다.\n아이디 혹은 비밀번호를 확인해주세요!");
-      
-      // 모달창 제거
-      setOpen(false);
     }
-  
+
     // form 잠금 해제
     setSent(false);
   };
@@ -111,28 +129,43 @@ export default function LoginModal(props) {
                 로그인
               </Typography>
               <input
-                class={classes.input}
+                id={"email"}
+                className={classes.input}
                 type={"text"}
-                onChange={changeEmail}
+                onClick={handleKeyboardOpen}
+                defaultValue={member.email}
                 placeholder="아이디"
                 autoFocus
                 maxLength="40"
                 disabled={sent}
               />
               <input
-                class={classes.input}
+                id={"password"}
+                className={classes.input}
                 type={"password"}
-                onChange={changePassword}
+                onClick={handleKeyboardOpen}
+                defaultValue={member.password}
                 placeholder="비밀번호"
                 maxLength="20"
                 disabled={sent}
               />
-              <Button onClick={onSubmit} variant="contained" className={classes.button} type="submit">
+
+              <Button
+                onClick={onSubmit}
+                variant="contained"
+                className={classes.button}
+                type="submit"
+              >
                 로그인
               </Button>
             </form>
           </div>
         </Fade>
+      </Modal>
+      <Modal open={keyboardOpen.open} onClose={handleKeyboardClose}>
+        <div className={classes.keyboard}>
+          <Keyboard onChange={onChangeKeyboard} maxLength={40} />
+        </div>
       </Modal>
     </div>
   );
