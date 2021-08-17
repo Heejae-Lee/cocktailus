8866253927614429e08e0a0015dd594a274abb2a
lktgt15 <lktgt15@gmail.com>
2021-08-17 16:29:00 +0900

    Feat: 'RecipeArticle 검색'

--- backend/cocktailer/src/main/java/com/iot/cocktailer/service/RecipeArticleService.java
+++ backend/cocktailer/src/main/java/com/iot/cocktailer/service/RecipeArticleService.java
@@ -5,8 +5,6 @@ import com.iot.cocktailer.domain.Like;
 import com.iot.cocktailer.domain.LikeId;
 import com.iot.cocktailer.domain.RecipeArticle;
 import com.iot.cocktailer.repository.JpaCommentRepository;
-import com.iot.cocktailer.repository.JpaLikeRepository;
-import com.iot.cocktailer.repository.JpaMemberRepository;
 import com.iot.cocktailer.repository.JpaRecipeArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,7 +32,7 @@ public class RecipeArticleService {
         this.memberService = memberService;
     }
 
-    public Map<String,Object> getRecipeArticle(Long id){
+    public Map<String,Object> getRecipeArticle(Long id,String jwt){
         Map<String,Object> result = new HashMap<>();
         Optional<RecipeArticle> optionalRecipeArticle = jpaRecipeArticleRepository.findById(id);
         RecipeArticle recipeArticle = optionalRecipeArticle.orElseThrow(()
@@ -42,8 +40,13 @@ public class RecipeArticleService {
         );
 
         // member liked
-        Like like = likeService.getLikeByLikeId(new LikeId(recipeArticle.getId(),recipeArticle.getMember_name()));
-        recipeArticle.setLiked(like.getLiked());
+        if(jwt != null) {
+            String member_name = memberService.getMembernameByJwt(jwt);
+
+            Like like = likeService.getLikeByLikeId(new LikeId(recipeArticle.getId(), member_name));
+            recipeArticle.setLiked(like.getLiked());
+        }
+
         List<Comment> comments = jpaCommentRepository.findByRecipeArticleId(id);
 
         result.put("recipe-article",recipeArticle);
@@ -51,20 +54,11 @@ public class RecipeArticleService {
         return result;
     }
 
-    public List<RecipeArticle> getRecipeArticles(String jwt){
+    public List<RecipeArticle> getRecipeArticlesWithLiked(String jwt){
         List<RecipeArticle> recipeArticles = jpaRecipeArticleRepository.findAll();
-        if(jwt != null) {
-            String member_name = memberService.getMembernameByJwt(jwt);
-
-            // member liked
-            for (RecipeArticle recipeArticle : recipeArticles) {
-                Like like = likeService.getLikeByLikeId(new LikeId(recipeArticle.getId(), member_name));
-                recipeArticle.setLiked(like.getId().getMember_name().equals(member_name) ? like.getLiked() : false);
-            }
-        }
-
-        return recipeArticles;
+        return getRecipeArticlesWithLiked(jwt, recipeArticles);
     }
+
 
     public RecipeArticle postRecipeArticle(RecipeArticle recipeArticle){
         String imageUrl = s3UploadService.upload(recipeArticle,"static");
@@ -126,5 +120,26 @@ public class RecipeArticleService {
                 ()-> new IllegalStateException("No matching id")
             );
         jpaRecipeArticleRepository.remove(recipeArticle);
+    }
+
+    public List<RecipeArticle> searchRecipeArticles(String title,String jwt){
+        List<RecipeArticle> recipeArticles = jpaRecipeArticleRepository.findByTitleDesc(title);
+
+        // member liked
+        return getRecipeArticlesWithLiked(jwt, recipeArticles);
+    }
+
+    private List<RecipeArticle> getRecipeArticlesWithLiked(String jwt, List<RecipeArticle> recipeArticles) {
+        if(jwt != null) {
+            String member_name = memberService.getMembernameByJwt(jwt);
+
+            // member liked
+            for (RecipeArticle recipeArticle : recipeArticles) {
+                Like like = likeService.getLikeByLikeId(new LikeId(recipeArticle.getId(), member_name));
+                recipeArticle.setLiked(like.getId().getMember_name().equals(member_name) ? like.getLiked() : false);
+            }
+        }
+
+        return recipeArticles;
     }
 }
