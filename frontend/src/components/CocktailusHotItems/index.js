05efa11b4577255daec4c9ae2032c619ea356649
이희재 <gmlwo1340@naver.com>
2021-08-18 00:41:59 +0900

    Feat: '홈 메뉴 반응형 기능 추가'
    새로 등록된, 트렌디한 칵테일 세로로 출력

--- frontend/src/components/CocktailusHotItems/index.js
+++ frontend/src/components/CocktailusHotItems/index.js
@@ -11,46 +11,58 @@ import Button from "../../components/Button";
 import RecipePreview from "../../components/RecipePreview";
 // 기능 관련
 import PropTypes from "prop-types";
-import { NavLink as RouterLink } from 'react-router-dom';
-
-import { recipeAPI } from '../../utils/recipeAPI';
+import { NavLink as RouterLink } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
+import { recipeAPI } from "../../utils/recipeAPI";
 
 function CocktailusHotItems(props) {
   const { classes } = props;
   const [recipes, setRecipes] = useState([]);
 
+  const isPc = useMediaQuery({
+    query: "(min-width:909px)",
+  });
+  const isMobile = useMediaQuery({
+    query: "(max-width:908px)",
+  });
+
   useEffect(() => {
     recipeAPI.getRecipes(setRecipes);
-  }, [])
+  }, []);
   return (
     <section className={classes.root}>
-      <Container className={classes.container}>
-        <Box style={{width: '100%', display: 'flex', justifyContent: 'space-between'}}>
-        {/* 높은 트렌드 칵테일 추천 타이틀 */}
-        <Typography
-          variant="h4"
-          marked="left"
-          className={classes.title}
-        >
-          트렌디한 칵테일
-        </Typography>
-        <Button
-          component={RouterLink}
-          color="secondary"
-          size="large"
-          className={classes.btn}
-          variant="contained"
-          to="/recipe/list/popular"
-        >
-            더 보러가기
-          </Button>
-        </Box>
-        {/* 트렌디한 칵테일 리스트 */}
-        <Grid className={classes.grid} container spacing={10} direction="row">
-        {recipes.slice(0,3).map(recipe => (
+      {/* pc 버전 충분한 가로 폭이 있을 경우 */}
+      {isPc && (
+        <Container className={classes.container}>
+          <Box
+            style={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            {/* 높은 트렌드 칵테일 추천 타이틀 */}
+            <Typography variant="h4" marked="left" className={classes.title}>
+              트렌디한 칵테일
+            </Typography>
+            <Button
+              component={RouterLink}
+              color="secondary"
+              size="large"
+              className={classes.btn}
+              variant="contained"
+              to="/recipe/list/popular"
+            >
+              더 보러가기
+            </Button>
+          </Box>
+          {/* 트렌디한 칵테일 리스트 */}
+          <Grid container spacing={10} direction="row">
+            {recipes.slice(0, 3).map((recipe) => (
               <RecipePreview
                 key={recipe.id}
-                id = {recipe.id}
+                style={{ width: "400px" }}
+                id={recipe.id}
                 image={recipe.image}
                 title={recipe.title}
                 content={recipe.content}
@@ -60,10 +72,53 @@ function CocktailusHotItems(props) {
                 imageURL={recipe.imageURL}
                 likeCount={recipe.likeCount}
                 liked={recipe.liked}
-                />
+              />
             ))}
-        </Grid>
-      </Container>
+          </Grid>
+        </Container>
+      )}
+      {/* 가로폭이 충분하지 않을 경우 */}
+      {isMobile && (
+        <Container className={classes.container}>
+          {/* 높은 트렌드 칵테일 추천 타이틀 */}
+          <div className={classes.columnTitle}>
+            <Typography variant="h4" marked="left" className={classes.title}>
+              트렌디한 칵테일
+            </Typography>
+          </div>
+          {/* 트렌디한 칵테일 리스트 */}
+          <div className={classes.grid}>
+            {recipes.slice(0, 3).map((recipe) => (
+              <div key={recipe.id} className={classes.recipe}>
+                <RecipePreview
+                  id={recipe.id}
+                  image={recipe.image}
+                  title={recipe.title}
+                  content={recipe.content}
+                  name={recipe.member_name}
+                  created={recipe.created}
+                  updated={recipe.updated}
+                  imageURL={recipe.imageURL}
+                  likeCount={recipe.likeCount}
+                  liked={recipe.liked}
+                />
+              </div>
+            ))}
+          </div>
+          <div className={classes.columnBtn}>
+            <Button
+              component={RouterLink}
+              color="secondary"
+              size="large"
+              className={classes.btn}
+              variant="contained"
+              to="/recipe/list/popular"
+            >
+              더 보러가기
+            </Button>
+          </div>
+        </Container>
+      )}
     </section>
   );
 }
--- frontend/src/components/CocktailusNewItems/index.js
+++ frontend/src/components/CocktailusNewItems/index.js
@@ -11,52 +11,63 @@ import Button from "../../components/Button";
 import RecipePreview from "../../components/RecipePreview";
 // 기능 관련
 import PropTypes from "prop-types";
-import { NavLink as RouterLink } from 'react-router-dom';
-
-import { recipeAPI } from '../../utils/recipeAPI';
+import { NavLink as RouterLink } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
+import { recipeAPI } from "../../utils/recipeAPI";
 
 function CocktailusNewItems(props) {
   const { classes } = props;
   const [recipes, setRecipes] = useState([]);
 
+  const isPc = useMediaQuery({
+    query: "(min-width:909px)",
+  });
+  const isMobile = useMediaQuery({
+    query: "(max-width:908px)",
+  });
+
   useEffect(() => {
     recipeAPI.getRecipes(setRecipes);
-  }, [])
+  }, []);
 
   return (
     <section className={classes.root}>
-      <Container className={classes.container}>
-        <img
-          src={process.env.PUBLIC_URL + "images/productCurvyLines.png"}
-          className={classes.curvyLines}
-          alt="curvy lines"
-        />
-        <Box style={{width: '100%', display: 'flex', justifyContent: 'space-between'}}>
-        {/* 새로 추가된 칵테일 추천 타이틀 */}
-        <Typography
-          variant="h4"
-          marked="left"
-          className={classes.title}
-        >
-          최근 등록된 칵테일
-        </Typography>
-        <Button 
-          component={RouterLink}
-          color="secondary"
-          size="large"
-          className={classes.btn}
-          variant="contained"
-          to="/recipe/list/new"
-        >
-            더 보러가기
-          </Button>
-        </Box>
-        {/* 트렌디한 칵테일 리스트 */}
-        <Grid className={classes.grid} container spacing={10} direction="row">
-          {recipes.slice(0,3).map(recipe => (
+      {/* pc 버전 충분한 가로 폭이 있을 경우 */}
+      {isPc && (
+        <Container className={classes.container}>
+          <img
+            src={process.env.PUBLIC_URL + "images/productCurvyLines.png"}
+            className={classes.curvyLines}
+            alt="curvy lines"
+          />
+          <Box
+            style={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            {/* 새로 추가된 칵테일 추천 타이틀 */}
+            <Typography variant="h4" marked="left" className={classes.title}>
+              최근 등록된 칵테일
+            </Typography>
+            <Button
+              component={RouterLink}
+              color="secondary"
+              size="large"
+              className={classes.btn}
+              variant="contained"
+              to="/recipe/list/new"
+            >
+              더 보러가기
+            </Button>
+          </Box>
+          {/* 새로 추가된 칵테일 리스트 */}
+          <Grid container spacing={10} direction="row">
+            {recipes.slice(0, 3).map((recipe) => (
               <RecipePreview
                 key={recipe.id}
-                id = {recipe.id}
+                id={recipe.id}
                 image={recipe.image}
                 title={recipe.title}
                 content={recipe.content}
@@ -66,10 +77,59 @@ function CocktailusNewItems(props) {
                 imageURL={recipe.imageURL}
                 likeCount={recipe.likeCount}
                 liked={recipe.liked}
-                />
+              />
             ))}
-        </Grid>
-      </Container>
+          </Grid>
+        </Container>
+      )}
+      {/* 가로폭이 충분하지 않을 경우 */}
+      {isMobile && (
+        <Container className={classes.container}>
+          {/* 최근 등록된 칵테일 추천 타이틀 */}
+          <img
+            src={process.env.PUBLIC_URL + "images/productCurvyLines.png"}
+            className={classes.curvyLines}
+            alt="curvy lines"
+          />
+          <div className={classes.columnTitle}>
+            {/* 최근 등록된 칵테일 추천 타이틀 */}
+            <Typography variant="h4" marked="left" className={classes.title}>
+              최근 등록된 칵테일
+            </Typography>
+          </div>
+          {/* 트렌디한 칵테일 리스트 */}
+          <div className={classes.grid}>
+            {recipes.slice(0, 3).map((recipe) => (
+              <div key={recipe.id} className={classes.recipe}>
+                <RecipePreview
+                  id={recipe.id}
+                  image={recipe.image}
+                  title={recipe.title}
+                  content={recipe.content}
+                  name={recipe.member_name}
+                  created={recipe.created}
+                  updated={recipe.updated}
+                  imageURL={recipe.imageURL}
+                  likeCount={recipe.likeCount}
+                  liked={recipe.liked}
+                />
+              </div>
+            ))}
+          </div>
+          <div className={classes.columnBtn}>
+            <Button
+              component={RouterLink}
+              color="secondary"
+              size="large"
+              className={classes.btn}
+              variant="contained"
+              to="/recipe/list/new"
+            >
+              더 보러가기
+            </Button>
+          </div>
+        </Container>
+      )}
     </section>
   );
 }
