58d29f52798a7c21189f0b3b144b4e72a0eb2ed7
이희재 <gmlwo1340@naver.com>
2021-08-18 14:30:14 +0900

    Feat: 'AboutUs 페이지 로티 애니메이션 추가'

--- frontend/src/pages/AboutUs/index.js
+++ frontend/src/pages/AboutUs/index.js
@@ -1,4 +1,5 @@
 import withRoot from '../../components/withRoot';
+import animationData from "../../lotties/character-walk.json";
 // --- Post bootstrap -----
 import React from 'react';
 import Container from '@material-ui/core/Container';
@@ -7,30 +8,63 @@ import Typography from '../../components/Typography';
 import AppHeader from '../../layout/Header';
 import AppFooter from '../../layout/Footer';
 import useStyles from './styles'
+import Lottie from "react-lottie";
 
 function AboutUs() {
   const classes = useStyles();
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   return (
     <React.Fragment>
       <AppHeader />
       <Container className={classes.root}>
-        <Box mt={7} mb={12}>
+        <Box mt={7}>
           <Typography variant="h2" gutterBottom marked="center" align="center">
             About Us
           </Typography>
-          <Typography variant="h6" gutterBottom align="center">
-            {"안녕하세요 Cocktail.Us 입니다."}
-            <div>.</div>
-            <div>{"이희재"}</div>
-            <div>.</div>
-            <div>{"방지환"}</div>
-            <div>.</div>
-            <div>{"신기호"}</div>
-            <div>.</div>
-            <div>{"임규태"}</div>
-            <div>.</div>
+          
+      <Lottie options={defaultOptions}
+              height={300}
+              width={500}/>
+              <Typography variant="h6" align="center" style={{ textDecoration: "overline", marginTop: 10}}>
+                {"안녕하세요 Cocktail.Us 입니다. "}
+              </Typography>
+          <Typography variant="body1" align="center">
+            {"저희와 칵테일 한잔 하시겠어요?"}
           </Typography>
+          
+            <Typography variant="h6" align="center" style={{marginTop: 40}}>이희재(FE)</Typography>
+            <Typography variant="subtitle1" align="center">
+              여기 칵테일 한잔 주세요! 참 알콜은 빼고요!
+            </Typography>
+            <Typography variant="subtitle2" align="center">gmlwo1340@naver.com</Typography>
+            <Typography variant="h6" align="center">.</Typography>
+            <Typography variant="h6" align="center">방지환(FE)</Typography>
+            <Typography variant="subtitle1" align="center">
+              무슨말을 해야할까!
+            </Typography>
+            <Typography variant="subtitle2" align="center">qkdwlghks00@gmail.com</Typography>
+            <Typography variant="h6" align="center">.</Typography>
+            <Typography variant="h6" align="center">임규태(BE)</Typography>
+            <Typography variant="subtitle1" align="center">
+              무슨말을 해야할까!
+            </Typography>
+            <Typography variant="subtitle2" align="center">lktgt15@gmail.com</Typography>
+            <Typography variant="h6" align="center">.</Typography>
+            <Typography variant="h6" align="center">신기호(EM)</Typography>
+            <Typography variant="subtitle1" align="center">
+              무슨말을 해야할까!
+            <Typography variant="subtitle2" align="center">rlgh944@naver.com</Typography>
+            </Typography>
+            <Typography variant="h6" align="center">.</Typography>
         </Box>
       </Container>
       <AppFooter />
--- frontend/src/pages/AboutUs/styles.js
+++ frontend/src/pages/AboutUs/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    paddingBottom: '14vh',
+    height: '800px'
   },
 }));
 
